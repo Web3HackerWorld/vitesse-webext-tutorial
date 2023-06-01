2fbd241efbcad879d3c8d194c7dcd232b4dc780d
Web3HackerWorld <web3hackerdao@gmail.com>
2023-06-01 15:19:03 +0800

    ✨ inject content script only in localhost

--- src/manifest.ts
+++ src/manifest.ts
@@ -43,7 +43,7 @@ export async function getManifest() {
     content_scripts: [
       {
         matches: [
-          '<all_urls>',
+          'http://localhost/*',
         ],
         js: [
           'dist/contentScripts/index.global.js',
